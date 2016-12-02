afa35d9e819e8e37f0f16d34858310c4e978520e
Evert <JE640817@swosu.edu>
2016-12-02 12:00:14 -0600

    added a character object.

---
+++ KillerKittens/src/GeneralCharacter.java
@@ -0,0 +1,29 @@
+
+public class GeneralCharacter {
+	
+	/* Title: classGeneralCharacter
+	 * 
+	 * Data:
+	 * 	-type: String
+	 * 	-alive: boolean 
+	 * 	-lifePoints: int
+	 * 	-attackPoints: int
+	 * 	-defensePoints: int
+
+	 */
+	
+	// Data
+	private String type = null;
+	private boolean alive = true;
+	private int lifePoints = 1;
+	private int attackPoints = 1;
+	private int defensePoints = 1;
+	
+	
+	/* 
+	 * Methods
+	 * 	+getLifePoints()
+	 * 	+setLifePoints()
+	 */
+
+}
