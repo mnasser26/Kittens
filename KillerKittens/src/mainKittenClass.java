2c332c914b61e27538c9ccb1a396e0337144e79f
Jeremy Evert <jeremy.evert@swosu.edu>
2016-12-01 22:55:34 -0600

    added a start to the program.

---
+++ KillerKittens/src/mainKittenClass.java
@@ -0,0 +1,21 @@
+public class mainKittenClass {
+	public static void main(String[] args) {
+		System.out.println("Hello Kittens.");
+		
+		/** Killer Kittens Main Class 
+		 * This is the main class for the killer kittens project.
+		 * This is a project to help CS 1 students
+		 * work on their github skills.
+		 * The project also helps the students work on their 
+		 * object oriented programming.
+		 * 
+		 * Project objectives for Jeremy: 
+		 * 1. Make a basic grunt object.
+		 * 2. Make a basic sniper object.
+		 * 3. Make a clan that has snipers and grunts.
+		 * 4. Make them do battle.
+		 */
+
+	}
+
+}
