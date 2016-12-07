bea917c99feaa8afc1967d65c0aa89a4de99853e
Lyle Schwemley <schwemley.lyle@gmail.com>
2016-12-07 11:42:02 -0600

    Lyle added data for the assassin class

--- KillerKittens/src/assassin.java
+++ KillerKittens/src/assassin.java
@@ -1,4 +1,41 @@
 
 public class assassin {
-
+	/* 
+	 * Assassin extends grunt 
+	 * Assassins have more skills than 
+	 * grunts but less than sergeants
+	 * 
+	 * Assassins follow sergeants orders
+	 * but only when it suits them. 
+	 * Assassins have been known to go 
+	 * MIA just because they can. 
+	 * They are arguably the most 
+	 * dangerous type of character. 
+	 * 
+	 * Assassins can do long range 
+	 * or sneak. 
+	 * 
+	 */
+	
+	/*  Data:
+	 * 	-type: String
+	 * 	-alive: boolean 
+	 * 	-lifePoints: int
+	 * 	-attackPoints: int
+	 * 	-defensePoints: int
+	 *  -sneak: int 
+	 *  -longRange: int 
+	 */
+	
+	//Data 
+	String type = null; 
+	boolean alive = true;
+	int lifePoints = 1; 
+	int attackPoints = 1; 
+	int defensePoints = 1; 
+	int sneak = 1; 
+	int longRange = 1; 
+	
+	
+	
 }
