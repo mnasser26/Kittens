69ec3ee1186781ad081ce74e1454d487ba8f8907
Jeremy Evert <jeremy.evert@swosu.edu>
2016-12-07 11:52:34 -0600

    Beardy

--- KillerKittens/src/Sergeant.java
+++ KillerKittens/src/Sergeant.java
@@ -4,13 +4,17 @@ public class Sergeant extends classGrunt {
 	/**Sergeant is the head of the party. He orders his troops to move around
 	 * Sergeant do not have a lot of armor, but grunts protect him
 	 * Assassins are lethal to Sergeant - that's what they are there for
+	 * Meaning that sergeant will always receive doubled damage from assassins
 	 * 
 	 * Their special abilities:
 	 * Call Retreat: When the battle does not favor them all troops retreat without
 	 * any consequences
+	 * 
 	 * Focus Attack: Double damage to all grunts for a while, but afterwards all the
 	 * grunts become more vulnerable
+	 * 
 	 * Veteran: Sergeant is a seasoned veteran, doubling all experience he might receive
+	 * 
 	 * Death Cry: If Sergeant gets killed, all grunts receive some f-ing overpowered buff
 	 * but if the match doesn't end soon the grunt will become near useless.
 	 *
